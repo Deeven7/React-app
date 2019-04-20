fb0e320c70dedcb264b9f94a57b643e6e2d090b9
Deeven Paul <pauldeeven7@gmail.com>
2019-04-20 13:18:53 -0400

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className = "container">
+            <h4 className = "center">About </h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta incidunt doloribus quidem magnam hic sint recusandae debitis tenetur culpa eaque quis dolore sunt quibusdam voluptates quae ex natus, optio iure!</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = (props) => {
+    return(
+        <div className = "container">
+            <h4 className = "center">Contact</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ipsa exercitationem impedit accusantium consequatur rem facere ipsum dicta excepturi ullam modi cumque quo nemo. Odio, doloribus. Sapiente corrupti voluptatibus suscipit.</p>
+        </div>
+    )
+}
+
+export default Contact;
